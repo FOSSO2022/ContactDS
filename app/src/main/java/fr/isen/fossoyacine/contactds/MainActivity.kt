8d40b5adf21afb341d6ceaedcefc63049dee007d
Yacine FOSSO <yacine-zorah.fosso-kenfack@isen.yncrea.fr>
2023-01-27 10:59:10 +0100

    Initial commit

---
+++ app/src/main/java/fr/isen/fossoyacine/contactds/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.fossoyacine.contactds
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
